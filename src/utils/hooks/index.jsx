33421c9740d7b53bdb374c5f4017efc30080700d
Yannick LEFAIVRE <debeug@outlook.fr>
2022-05-27 08:35:08 -0400

    refactor(hooks): move fetchData() into call to useEffect()

--- src/utils/hooks/index.jsx
+++ src/utils/hooks/index.jsx
@@ -5,24 +5,23 @@ export function useFetch(url) {
   const [data, setData] = useState([])
   const [error, setError] = useState(false)
 
-  const fetchData = async () => {
-    setLoading(true)
-    try {
-      const response = await fetch(url)
-      const data = await response.json()
-
-      setData(data)
-    } catch (e) {
-      console.log('==== error ====', e)
-      setError(true)
-    } finally {
-      setLoading(false)
-    }
-  }
-
   useEffect(() => {
+    setLoading(true)
+    const fetchData = async () => {
+      try {
+        const response = await fetch(url)
+        const data = await response.json()
+
+        setData(data)
+      } catch (e) {
+        console.log('==== error ====', e)
+        setError(true)
+      } finally {
+        setLoading(false)
+      }
+    }
     fetchData()
-  }, [])
+  }, [url])
 
   return { isLoading, data, error }
 }
